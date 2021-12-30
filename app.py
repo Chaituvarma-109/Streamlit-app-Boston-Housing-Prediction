436b5a2f45432248224c9eb4dba8f5c0e2eb5010
chaituvarma-109 <chaitanyavarma.u@gmail.com>
2021-12-30 19:28:34 +0530

    modified the app file

--- app.py
+++ app.py
@@ -3,7 +3,7 @@ import pickle
 
 from sklearn.linear_model import LinearRegression
 
-st.set_page_config(page_title="Boston Housing Price Prediction", layout="wide")
+st.set_page_config(page_title="Boston Housing Price Prediction")
 
 
 def main():
@@ -12,19 +12,20 @@ def main():
     input_cont = st.container()
 
     with input_cont:
-        indus = st.number_input("Enter the value")
-        rm = st.number_input("Enter the no. of rooms")
-        tax = st.number_input("Enter the tax rate")
-        ptratio = st.number_input("Enter the people teacher ratio")
-        lstat = st.number_input("Enter the lstat")
+        indus = st.number_input("INDUS value")
+        rm = st.number_input("RM value")
+        tax = st.number_input("TAX rate")
+        ptratio = st.number_input("PT RATIO Value")
+        lstat = st.number_input("LSTAT Value")
 
-        filename = 'model.pickle'
-        model = pickle.load(open(filename, 'rb'))
-        prediction = model.predict([[indus, rm, tax, ptratio, lstat]])
+    filename = 'model.pickle'
+    model = pickle.load(open(filename, 'rb'))
+    prediction = model.predict([[indus, rm, tax, ptratio, lstat]])
 
-        # 6.91, 6.211, 233.0, 17.9, 7.44
-        # 18.10,5.852,666.0,20.2,29.97
-        st.info(f"Model prediction is: {round(prediction[0], 2)}")
+    # 6.91, 6.211, 233.0, 17.9, 7.44
+    # 18.10,5.852,666.0,20.2,29.97
+    st.subheader("Predicted Output")
+    st.info(f"Model Predicted Price is: {round(prediction[0], 2)}")
 
 
 if __name__ == "__main__":
